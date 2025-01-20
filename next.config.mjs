5eabdaec0defe7dc3023320f6f60cc481e100132
abdulazeezsodiq <sodiq.asynctechs@gmail.com>
2025-01-20 22:29:00 +0100

    fixed...

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,48 @@
+let userConfig = undefined
+try {
+  userConfig = await import('./v0-user-next.config')
+} catch (e) {
+  // ignore error
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    ignoreDuringBuilds: true, // Avoid build failure due to linting issues
+    ignoreDuringBuilds: true,
   },
   typescript: {
-    ignoreBuildErrors: true, // Prevent build failures due to TypeScript errors
+    ignoreBuildErrors: true,
   },
   images: {
-    unoptimized: true, // Use unoptimized images for easier deployment
+    unoptimized: true,
   },
-};
+  experimental: {
+    webpackBuildWorker: true,
+    parallelServerBuildTraces: true,
+    parallelServerCompiles: true,
+  },
+}
 
-export default nextConfig;
+mergeConfig(nextConfig, userConfig)
+
+function mergeConfig(nextConfig, userConfig) {
+  if (!userConfig) {
+    return
+  }
+
+  for (const key in userConfig) {
+    if (
+      typeof nextConfig[key] === 'object' &&
+      !Array.isArray(nextConfig[key])
+    ) {
+      nextConfig[key] = {
+        ...nextConfig[key],
+        ...userConfig[key],
+      }
+    } else {
+      nextConfig[key] = userConfig[key]
+    }
+  }
+}
+
+export default nextConfig
